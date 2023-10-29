cd96c13338523589658a933506b2422720bd91b1
Sea10wood <miyusea5@gmail.com>
2023-10-29 14:00:09 +0900

    route にmap_set.dartの追加

--- fukutra_app/lib/router/route.dart
+++ fukutra_app/lib/router/route.dart
@@ -5,6 +5,7 @@ import 'package:fukutra_app/view/confirmuser_screen.dart';
 import 'package:fukutra_app/view/forget_screen.dart';
 import 'package:fukutra_app/view/home_screen.dart';
 import 'package:fukutra_app/view/login_screen.dart';
+import 'package:fukutra_app/view/map_set.dart';
 import 'package:fukutra_app/view/move_set.dart';
 import 'package:fukutra_app/view/people_set.dart';
 import 'package:fukutra_app/view/place_set.dart';
@@ -54,6 +55,17 @@ final routerProvider = Provider<GoRouter>((ref) {
       GoRoute(
         path: '/peopleset',
         pageBuilder: (context, state) => const MaterialPage(child: PeopleSet()),
+        redirect: (context, state) {
+          final result = isUserSignedIn().toString();
+          if (result == "false") {
+            return '/';
+          }
+          return null;
+        },
+      ),
+      GoRoute(
+        path: '/map',
+        pageBuilder: (context, state) => const MaterialPage(child: MapSet()),
         redirect: (context, state) {
           final result = isUserSignedIn().toString();
           if (result == "false") {
